1a4f12a0433496ef03530f137747d43f9324f583
Vladislav Dovzhenko <vladka.dovzh@gmail.com>
2023-11-29 16:48:46 +0300

    chore(*): css modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,6 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import path from "path";
 import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
@@ -11,5 +11,9 @@ export function buildPlugins({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
     ];
 }
